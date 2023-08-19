03c011d3c4154870c34b91bfc4cc796dbf5401c2
Basant Pandey <basantwelcome2you@gmail.com>
2023-08-19 18:48:51 +0530

    revert chagnes

--- index.js
+++ index.js
@@ -7,17 +7,6 @@ const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'post.json'))
 const middlewares = jsonServer.defaults()
 
-var whitelist = ['http://localhost:3000']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
 
 server.use(cors(corsOptions))
 server.use(jsonServer.bodyParser)
